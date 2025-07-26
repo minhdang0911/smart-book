35ffe367822b1767979bd0ebbf38f2c7709a6fae
minhdang0911 <121071062+minhdang0911@users.noreply.github.com>
2025-07-26 14:01:57 +0700

    update

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -1,16 +1,7 @@
 import { Suspense } from 'react';
 import dynamic from 'next/dynamic';
 import { Spin } from 'antd';
-
-const AuthPage = dynamic(() => import('./AuthPageContent'), {
-  ssr: false,
-  loading: () => (
-    <div className="flex justify-center items-center min-h-screen">
-      <Spin size="large" />
-    </div>
-  ),
-});
-
+import AuthPage from './AuthPageContent';
 export default function AuthPageWrapper() {
   return (
     <Suspense fallback={<div className="text-center mt-20">Đang tải...</div>}>
