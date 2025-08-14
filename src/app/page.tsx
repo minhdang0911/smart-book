7410aca1db62a1b873b5fdd9ec4c5351139c85a6
minhdang0911 <121071062+minhdang0911@users.noreply.github.com>
2025-08-14 10:25:34 +0700

    update logic banner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import Author from './components/Author/Author';
-import Banner from './components/banner/banner';
+import VoyageSlider from './components/banner/banner';
 import Blog from './components/blog/blog';
 import BookAIChatbot from './components/Chatbot/BookAIChatbot';
 import CouponSlider from './components/coupon/coupon';
@@ -7,18 +7,17 @@ import Event from './components/event/page';
 import Gift from './components/Gift/gift';
 import Product from './components/product/page';
 import BookRunningBanner from './components/RunningBookBanner';
+
 export default function Home() {
     return (
         <div>
-            <Banner />
-
+            <VoyageSlider />
             <CouponSlider />
             <Event />
             <Author />
             <Product />
             <Gift />
             <Blog />
-
             <BookAIChatbot />
             <div style={{ width: '100%' }}>
                 <BookRunningBanner />
